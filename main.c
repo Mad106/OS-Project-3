9860e40bd048e660cc28233a11cdbb9fa30cffaf
Ryan Lazzari <rgl12@my.fsu.edu>
2021-04-02 03:12:32 -0400

    Update to main.c

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
-#include <cstdio>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define READSIZE 32
+#define FIRSTDATASECTOR 2050
+
+struct DIRENTRY {
+	unsigned char DIR_Name[11];
+	unsigned char DIR_Attr[1];
+	unsigned char DIR_NTRes[1];
+	unsigned char DIR_CrtTimeTenth[1];
+	unsigned char DIR_CrtTime[2];
+	unsigned char DIR_CrtDate[2];
+	unsigned char DIR_LstAccDate[2];
+	unsigned char DIR_FstClusHI[2];
+	unsigned char DIR_WrtTime[2];
+	unsigned char DIR_WrtDate[2];
+	unsigned char DIR_FstClusLo[2];
+	unsigned char DIR_FileSize[4];
+} __attribute__((packed));
+
+/* Function Prototypes */
+int FileOffset(int clusterNum);
+int FileExists(const unsigned char* fName, int fd); // ret 1 if file exists, else 0
+void ReadEntry(struct DIRENTRY* entry, int fd);
 
 int main(int argc, char *argv[]){
 	/* run accept .img then wait for user to input commands and respond accordingly */
+
+	/* open fat32 image (argv[1] with read/write permissions */
+	/* fd tracks position inside fat32 image, reads data into buff */
+	int fd;
+	fd = open(argv[1], 2);
+
+	/* if input file didn't open, print error and quit */
+	if(fd < 0){
+		printf("File %s not found", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	
-	//take in/open .img
-	FILE input = fopen(argv[1],"r+");
-	
-	char* command;
-	
-	while(command != "exit"){
-		
-		print("$");
-		read_from_stdin(command);
+	/* user command input */
+	char* command = (char*)malloc(sizeof(char*));
+	printf("$");
+	scanf("%s", command);
+
+	while(strcmp("exit", command) != 0){
+		/* prompt and obtain user input */	
+		printf("$");
+		scanf("%s", command);
 		
 		if(command == "info"){
 			
@@ -46,8 +81,42 @@ int main(int argc, char *argv[]){
 		}
 		
 	}
-	
-	
-	
+		
 	return 0;
 }
+
+int FileOffset(int clusterNum)
+{
+	return (FIRSTDATASECTOR + (clusterNum-2))*512;
+}
+
+int FileExists(const unsigned char* fName, int fd)
+{
+	struct DIRENTRY* entry = (struct DIRENTRY*) malloc(sizeof(struct DIRENTRY*));
+
+	/* go to fileContents in fd */
+	lseek(fd, FileOffset(2), SEEK_SET);
+
+	/* read into DIR_entry struct. Find fName entry */
+	/* If long file entry, skip. */
+
+	/* LEFT OFF HERE */
+}
+
+
+void ReadEntry(struct DIRENTRY* entry, int fd)
+{
+	read(fd, entry->DIR_Name, 11);
+	read(fd, entry->DIR_Attr, 1);
+	read(fd, entry->DIR_NTRes, 1);
+	read(fd, entry->DIR_CrtTimeTenth, 1);
+	read(fd, entry->DIR_CrtTime, 2);
+	read(fd, entry->DIR_CrtDate, 2);
+	read(fd, entry->DIR_LstAccDate, 2);
+	read(fd, entry->DIR_FstClusHI, 2);
+	read(fd, entry->DIR_WrtTime, 2);
+	read(fd, entry->DIR_WrtDate, 2);
+	read(fd, entry->DIR_FstClusLo, 2);
+	read(fd, entry->DIR_FileSize, 4);
+}
+
